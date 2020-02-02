ef54631e7d339b96bbbc64d0332ba5ffabc77ca7
agnelD <agnieszka.duch@gmail.com>
2020-02-02 16:03:58 +0100

    install a SwiftyJSON library

--- AgaHub/Controllers/UserDetailsViewController.swift
+++ AgaHub/Controllers/UserDetailsViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class UserDetailsViewController: UITableViewController{
+class UserDetailsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
---
+++ AgaHub/UserDetails.swift
@@ -0,0 +1,15 @@
+//
+//  UserDetails.swift
+//  AgaHub
+//
+//  Created by Agnieszka Duch on 02/02/2020.
+//  Copyright © 2020 Agnieszka Duch. All rights reserved.
+//
+
+import Foundation
+
+struct UserDetails {
+    let avatar: UIImage
+    let userLogin: String
+    let userRepoLink: String
+}
